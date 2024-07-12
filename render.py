d8cc33f196c94841110c5b3741df0cd882c6cf77
Andrew Hernandez <121366254+ApaxPhoenix@users.noreply.github.com>
2024-07-12 13:56:47 -0400

    Update render.py

--- render.py
+++ render.py
@@ -1,7 +1,10 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from typing import List
+
+matplotlib.rcParams['toolbar'] = 'None'
 
 
 class Render:
@@ -14,6 +17,7 @@ class Render:
         """Animate frames using matplotlib."""
         figure = plt.figure()
         plt.axis('off')
+        figure.canvas.manager.set_window_title('Paint Grid')
         images = [[plt.imshow(frame, animated=True)] for frame in self.frames]
         _ = animation.ArtistAnimation(figure, images, interval=np.divide(1000, self.fps), blit=True)
         plt.show()
